29ca087442ceb817c3ceb1b510b2db150b04a2f9
Nathan Ward <nward@terafuze.com>
2019-05-18 15:04:31 -0400

    Added Go Home Notes specific Authorities/Roles to database and Java code.

--- src/main/java/com/terafuze/gohomenotes/security/AuthoritiesConstants.java
+++ src/main/java/com/terafuze/gohomenotes/security/AuthoritiesConstants.java
@@ -10,6 +10,18 @@ public final class AuthoritiesConstants {
     public static final String USER = "ROLE_USER";
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+    
+    public static final String PARENT = "ROLE_PARENT";
+    
+    public static final String SCHOOL_STAFF = "ROLE_SCHOOL_STAFF";
+    
+    public static final String STUDENT = "ROLE_STUDENT";
+    
+    public static final String PARENT_VOLUNTEER = "ROLE_PARENT_VOLUNTEER";
+    
+    public static final String TEACHER = "ROLE_TEACHER";
+    
+    public static final String SCHOOL_ADMIN = "ROLE_SCHOOL_ADMIN";
 
     private AuthoritiesConstants() {
     }
